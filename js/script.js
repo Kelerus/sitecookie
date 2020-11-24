1073295c5b0147c175686825f8aa36bc3908024d
Kelerus <11herobrineplay@mail.ru>
2020-11-24 22:21:38 +0400

    init

--- js/script.js
+++ js/script.js
@@ -36,28 +36,54 @@ $(document).ready(function() {
     })
 
     
-        if($.cookie('i') == null) {
+        if($.cookie('i') == null || $.cookie('masscookie') == null) {
             let i = 0;
             $.cookie('i', i);
+
+            let masscookie = "";
+            $.cookie('masscookie', masscookie)
+            
         }
         else {
-            $.cookie('i')
-            console.log($.cookie('i'))
+            let masscookie = $.cookie('masscookie');
+            let arrmasscookie = masscookie.split(',');
+            console.log(arrmasscookie);
+            if(arrmasscookie != ""){
+                arrmasscookie.forEach(function(element, index) {
+                    console.log(index)
+                    element = JSON.parse($.cookie(`${element}`))
+                    if(element != null) {
+                        $('.table__items').append(element);
+                    }
+                });
+            }
+
         }
         
-    for (let index = 0; index < $.cookie('i'); index++) {
+    /* for (let index = 0; index < $.cookie('i'); index++) {
         let element = JSON.parse($.cookie(`table${index}`));
         if(element != null){
             $('.table__items').append(element);
         }
         
-    }
+    } */
 
 
     $('#log').click(function() {
         let i = $.cookie('i')
-        let tablei = Number(i);
+        let tablei = Number(i);        
+        let masscookie = $.cookie('masscookie');
+        if(tablei > 0){
+            masscookie += `,table${tablei}`;
+            $.cookie('masscookie', masscookie);
+        }
+        else {
+            masscookie = `table${tablei}`;
+            $.cookie('masscookie', masscookie);
+        }
+
         
+
         let odata = {
             name: `${$('#inputname').val()}`,
             surName: `${$('#inputsurname').val()}`,
@@ -89,31 +115,31 @@ $(document).ready(function() {
             $.cookie('i', tablei);
             console.log(tablei);
         }
-        else {
-            let html = $('.table__items').html(`
-            <tr id="items-${tablei}">
-            <th scope="row">${tablei}</th>
-            <td>${odata.name}</td>
-            <td>${odata.surName}</td>
-            <td>${odata.number}</td>
-            <td><input numid="${tablei}" class="btntabledelete btn btn-danger" type="submit" value="Удалить"></td>
-            </tr>
-            `)
-            let jhtml = JSON.stringify(html);
-            $.cookie(`table${tablei++}`, jhtml);
-            $.cookie('i', tablei);
-            console.log(tablei);
-        }
     })
 
     $('.btntabledelete').click(function() {
         let cookiei = $.cookie('i');
         let _this = $(this);
         let id = _this.attr('numid');
-        $(`#items-${id}`).remove();
+       /*  $(`#items-${id}`).remove();
         $.removeCookie(`table${id}`);
         let deletei = cookiei - 1;
-        cookiei = $.cookie('i', deletei);
+        cookiei = $.cookie('i', deletei); */
+
+        let masscookie = $.cookie('masscookie');
+        let arrmasscookie = masscookie.split(',');
+        arrmasscookie.forEach(function(element, id) {
+            let checkid = _this.attr('numid');
+            if(checkid == id) {
+                $(`#items-${id}`).remove();
+                $.removeCookie(`table${checkid}`);
+                let deletei = cookiei - 1;
+                cookiei = $.cookie('i', deletei);
+                arrmasscookie.splice(checkid, 1);
+                $.cookie('masscookie', arrmasscookie)
+                console.log($.cookie('masscookie'));
+            }
+        });
     })
 
 })
---
+++ js/scriptmod.js
@@ -0,0 +1,132 @@
+$(document).ready(function() {
+
+    if($.cookie('auth') == null) {
+        $('.register_check').css("display", "flex");
+        $('.main').hide();
+    }
+    else {
+
+        let jpoauth = JSON.parse($.cookie('auth'));
+
+        $('.header_login').text(jpoauth.login);
+        
+        $('header').show();
+        $('.main').show();
+    }
+
+    $('#regbutton').click(function() {
+        let oauth = {
+            login: `${$('#exampleInputEmail1').val()}`,
+            password: `${$('#exampleInputPassword1').val()}`,
+            auth: `${Math.random()}`
+        }
+        let joauth = JSON.stringify(oauth);
+        $.cookie('auth', joauth);
+    })
+
+    $('#createbtn').click(function() {
+        if($('.create_table').attr('num_show') == '1'){
+            $('.create_table').hide();
+            $('.create_table').removeAttr('num_show');
+        }
+        else {
+            $('.create_table').show();
+            $('.create_table').attr('num_show', '1');
+        }
+    })
+
+    if($.cookie('id') == null || $.cookie('masscookie') == null) {
+        let id = 0;
+        let masscookie = "";
+        $.cookie('id', id);
+        $.cookie('masscookie', masscookie);
+    }
+
+    if($.cookie('masscookie') != "") {
+        let masscookie = $.cookie('masscookie') 
+        let arrmasscookie = masscookie.split(',');
+        try{
+            arrmasscookie.forEach(function(element) {
+                if(element != null) {
+                    
+                    let tablecookie = $.cookie(`${element}`);
+                    let parsetable = JSON.parse(tablecookie);
+                    $('.table__items').append(parsetable);
+                }
+            });
+        }
+        catch {
+            console.log('gg');
+        }
+    }
+
+
+    $('#log').click(function() {
+        let id = $.cookie('id');
+        let masscookie = $.cookie('masscookie');
+        
+
+        if(masscookie == "") {
+            
+            let masscookieid = `table${id}`
+            $.cookie('masscookie', masscookieid);
+            
+        }
+        else {
+            
+            let masscookieid = `table${id}`
+            masscookie += `,${masscookieid}`
+            $.cookie('masscookie', masscookie);
+
+        }
+        
+        let odata = {
+            name: `${$('#inputname').val()}`,
+            surName: `${$('#inputsurname').val()}`,
+            number: `${$('#inputnumber').val()}`
+        }
+        let html = 
+            `<tr id="items-${id}">
+            <th scope="row">${id}</th>
+            <td>${odata.name}</td>
+            <td>${odata.surName}</td>
+            <td>${odata.number}</td>
+            <td><input numid="${id}" class="btntabledelete btn btn-danger" type="submit" value="Удалить"></td>
+            </tr>`
+
+        $('.table__items').append(html);
+        
+
+        let jhtml = JSON.stringify(html);
+        $.cookie(`table${id++}`, jhtml);
+        $.cookie('id', id);
+    })
+
+    $('.btntabledelete').click(function() {
+        let numid = $(this).attr('numid');
+        $(`.items-${numid}`).remove();
+        let masscookie = $.cookie('masscookie');
+        let arrmasscookie = masscookie.split(',');
+        arrmasscookie.forEach(function(element) {
+            if(element == `table${numid}`) {
+                console.log(element);
+                console.log(numid);
+                let id = $.cookie('id');
+                id = id-1;
+                arrmasscookie.splice(numid, 1);
+                $.cookie('id', id);
+                $.cookie('masscookie', arrmasscookie);
+                if(arrmasscookie.length == 1) {
+                    $.removeCookie('masscookie');
+                    $.removeCookie('id');
+                }
+                else if(element != `table${numid}`) {
+                    console.log('ошибка');
+                    $.removeCookie('masscookie');
+                    $.removeCookie('id');
+                }
+            }
+        });
+        $.removeCookie(`table${numid}`);
+    })
+})
